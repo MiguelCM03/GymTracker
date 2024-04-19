8d8fd72e4952d448be654e90a253a3d8e163df26
miguelcm03 <miguelangelcanomorales191@gmail.com>
2024-04-19 22:03:28 +0200

    Mejora en la validacion de contraseña y la interfaz de creacion de cuenta

--- app/src/main/java/com/example/gymtracker/CrearCuentaActivity.kt
+++ app/src/main/java/com/example/gymtracker/CrearCuentaActivity.kt
@@ -10,10 +10,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 
-var txtUsuario: EditText? = null
-var txtContrasena: EditText? = null
+var txtCorreo: EditText? = null
+var txtContrasena: TextInputEditText? = null
 class CrearCuentaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,17 +26,17 @@ class CrearCuentaActivity : AppCompatActivity() {
             insets
         }
 
-        txtUsuario = findViewById(R.id.txtUsuario)
+        txtCorreo = findViewById(R.id.txtCorreo)
         txtContrasena = findViewById(R.id.txtContrasena)
-        val btnConfirmar = findViewById<Button>(R.id.btnCrearCuenta)
+        var btnConfirmar = findViewById<Button>(R.id.btnCrearCuenta)
         btnConfirmar.setOnClickListener{crearCuenta()}
-
     }
 
+    //Funcion actualmente inutil
     fun insertarDatos(view: View){
         var conn = SQLite(this, "Clientes", null, 1)
         var baseDatos = conn.writableDatabase
-        var wsNombre = txtUsuario?.text.toString()
+        var wsNombre = txtCorreo?.text.toString()
         var wsContrasena = txtContrasena?.text.toString()
         if(!wsNombre.isEmpty() && !wsContrasena.isEmpty()){
             var registro = ContentValues()
@@ -44,7 +45,7 @@ class CrearCuentaActivity : AppCompatActivity() {
             baseDatos.execSQL("SELECT * from USERS where UPPER(nombre) like UPPER()")
             baseDatos.insert("USERS", null, registro)
             txtContrasena?.setText("")
-            txtUsuario?.setText("")
+            txtCorreo?.setText("")
             Toast.makeText(this, "Registro añadido con exito", Toast.LENGTH_LONG).show()
         }else{
             Toast.makeText(this, "Campos vacíos", Toast.LENGTH_LONG).show()
@@ -55,7 +56,7 @@ class CrearCuentaActivity : AppCompatActivity() {
     fun crearCuenta(){
         if(validarContrasena(txtContrasena?.text.toString())) {
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(
-                txtUsuario?.text.toString(),
+                txtCorreo?.text.toString(),
                 txtContrasena?.text.toString()
             ).addOnCompleteListener { it ->
                 if (it.isSuccessful) {
@@ -65,7 +66,7 @@ class CrearCuentaActivity : AppCompatActivity() {
                 }
             }//On complete listener para saber si lo hace correctamente
         }
-    }
+    }//crearCuenta()
 
     fun validarContrasena(isContrasena: String):Boolean{
         var tieneMinus = false
@@ -73,14 +74,27 @@ class CrearCuentaActivity : AppCompatActivity() {
         var tieneNums = false
         var tieneSignos = false
         var contrasenaValida = false
+        var longitudCorrecta = true
+        if(txtContrasena?.text.toString().length < 6) { longitudCorrecta = false } //La contraseña es demasiado corta(FireBase necesita al menos 6 caracteres)
         for(i in isContrasena){
             if(i > 64.toChar() && i < 91.toChar())        tieneMayus  = true //La contraseña tiene al menos una mayúscula
             else if(i > 96.toChar() && i < 123.toChar())  tieneMinus  = true //La contraseña tiene al menos una minúscula
             else if(i > 47.toChar() && i < 58.toChar())   tieneNums   = true //La contraseña tiene al menos un número
             else                                          tieneSignos = true //La contraseña tiene al menos un carácter especial
         }
-        if(tieneMayus && tieneMinus && tieneNums && tieneSignos) contrasenaValida = true
+        if(!tieneMinus)       { Toast.makeText(this, "La contraseña debe tener al menos 1 minúscula",         Toast.LENGTH_LONG).show() }
+        else if(!tieneMayus)       { Toast.makeText(this, "La contraseña debe tener al menos 1 mayúscula",         Toast.LENGTH_LONG).show() }
+        else if(!tieneNums)        { Toast.makeText(this, "La contraseña debe tener al menos 1 número",            Toast.LENGTH_LONG).show() }
+        else if(!tieneSignos)      { Toast.makeText(this, "La contraseña debe tener al menos 1 carácter especial", Toast.LENGTH_LONG).show() }
+        else if(!longitudCorrecta) { Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres",        Toast.LENGTH_LONG).show() }
+
+
+
+
+        if(tieneMayus && tieneMinus && tieneNums && tieneSignos && longitudCorrecta) contrasenaValida = true
         return contrasenaValida
-    }
+    }//validarContrasena
+
+
 
 }//CrearCuentaActivity
