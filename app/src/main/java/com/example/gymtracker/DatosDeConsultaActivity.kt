ad7d3a98a773ae10e610268fc3f65b50eb7feb9c
miguelcm03 <miguelangelcanomorales191@gmail.com>
2024-05-27 20:14:43 +0200

    Creacion interfaz en la que se van a mostrar los datos consultados

--- app/src/main/java/com/example/gymtracker/DatosDeConsultaActivity.kt
+++ app/src/main/java/com/example/gymtracker/DatosDeConsultaActivity.kt
@@ -1,6 +1,7 @@
 package com.example.gymtracker
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,15 @@ class DatosDeConsultaActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val tvValorMesConsultado = findViewById<TextView>(R.id.tvValorMesConsultado)
+        val tvValorEjercicioConsultado = findViewById<TextView>(R.id.tvValorEjercicioConsultado)
+        val tvValorAnoConsultado = findViewById<TextView>(R.id.tvValorAnoConsultado)
+
+        val anoConsultado = intent.getStringExtra("ANO")
+        val mesConsultado = intent.getStringExtra("MES")
+        val ejercicioConsultado = intent.getStringExtra("EJERCICIO")
+        tvValorMesConsultado.setText(mesConsultado)
+        tvValorEjercicioConsultado.setText(ejercicioConsultado)
+        tvValorAnoConsultado.setText(anoConsultado)
     }
 }
